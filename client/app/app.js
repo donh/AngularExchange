746dedae6ec8671f1d0630553d6a3fa564571eca
Don Hsieh <don@kindminds.com>
2015-04-02 21:03:39 +0800

    OPEN - issue WD-119: Implement New Header + Tab Design of Sponsor Page https://simplybridal.atlassian.net/browse/WD-119

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('citiApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'ui.bootstrap'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
